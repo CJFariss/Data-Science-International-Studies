e55e80aec0392d2f5929f8a85baf2b0938432821
Christopher Fariss <cjf0006@gmail.com>
2023-04-17 21:56:17 -0400

    Update R_DSIS_week15_extra_stuff.R

--- DSIS_Lessons/R_DSIS_week15_extra_stuff.R
+++ DSIS_Lessons/R_DSIS_week15_extra_stuff.R
@@ -23,40 +23,68 @@
 ## Introduction to tutorial:
 ## For this R tutorial, we will learn:
 ##
-## (1) learn about the "pipe" operator and other tidyverse stuff
+## (1a) learn about the "pipe" operator |> for base R (this is only available in R versions 4.0 or greater)
+## (1b) learn about the "pipe" operator %>% for the tidyverse 
 ##
 ##########################################################################
 
 library(tidyverse)
 
-## simulate two variables and storing 2 variables in a matrix
-x <- matrix(NA, ncol=2, nrow=1000)
-x[,1] <- rnorm(1000)
-x[,2] <- rnorm(1000)
+## set the number of rows and columns for the plot
+par(mfrow=c(3,2))
 
+
+SIMS <- 500
+
+##########################################################################
+## base R examples
+##########################################################################
+
+## simulate two variables and storing 2 variables in a matrix
+x <- matrix(NA, ncol=2, nrow=SIMS)
+x[,1] <- rnorm(SIMS)
+x[,2] <- rnorm(SIMS)
 ## plot the 2 columns in the x matrix
-par(mfrow=c(2,2))
 plot(x, col=2)
 
 ## more compact version of simulating and storing 2 variables in a matrix
-x <- matrix(rnorm(1000*2), ncol=2, nrow=1000)
+x <- matrix(rnorm(SIMS*2), ncol=2, nrow=SIMS)
 
 ## plot the 2 columns in the x matrix
 plot(x, col=3)
 
 
-## using the pipe operator
-x <- rnorm(1000) %>%
-    matrix(ncol = 2)
+##########################################################################
+## base R examples using the base pipe operator
+##########################################################################
 
+## using the base pipe operator
+x <- rnorm(SIMS) |>
+  matrix(ncol = 2)
 ## plot the 2 columns in the x matrix
 plot(x, col=4)
 
+## do everything at once and don't save the x matrix using the base pipe operator
+rnorm(SIMS) |>
+  matrix(ncol = 2) |>
+  plot(col=1) 
 
-## do everything at once and don't save the x matrix
-rnorm(1000) %>%
+
+##########################################################################
+## base R examples using the tidyverse pipe operator
+##########################################################################
+
+## using the tidyverse operator
+x <- rnorm(SIMS) %>%
+    matrix(ncol = 2)
+## plot the 2 columns in the x matrix
+plot(x, col=5)
+
+## do everything at once and don't save the x matrix using the tidyverse pipe operator
+rnorm(SIMS) %>%
   matrix(ncol = 2) %>%
-  plot() 
+  plot(col=6) 
+
 
 
 
