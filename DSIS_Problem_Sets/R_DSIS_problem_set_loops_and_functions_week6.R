0eadaff2efbeb2f74ba88feb87c7c926505bde43
Christopher Fariss <cjf0006@gmail.com>
2025-03-18 18:14:56 -0400

    Create R_DSIS_problem_set_loops_and_functions_week6.R

---
+++ DSIS_Problem_Sets/R_DSIS_problem_set_loops_and_functions_week6.R
@@ -0,0 +1,60 @@
+## R_DSIS_problem_set_loops_and_functions_week6.R
+##########################################################################
+## INSTRUCTOR: Christopher Fariss
+## COURSE NAME: Data Science for International Studies (DSIS)
+## University of Michigan, Winter 2025, Fall 2023, Winter 2023, Winter 2022
+##
+## Week 5 / Week 6 / Week 7
+##
+## Please e-mail me if you find any errors or have and suggestions (either email is fine)
+## e-mail: cjf0006@gmail.com
+## e-mail: cjfariss@umich.edu
+##########################################################################
+## Instructions:
+##
+## This is an optional problem set that covers material in Davies Ch.8.
+## Please feel free to try any or all of the questions.
+## I am happy to go over each question with you individually during office hours.
+##
+##
+##########################################################################
+## Problem 1
+##########################################################################
+## (1a) Write a for loop to repeat 10 times and print a number starting at 1 and ending at 10
+## (1b) Write a for loop to repeat times and print a number starting at 11 and ending at 20
+## (1c) Write a for loop to repeat 10 times and print a number starting at 1 and ending at 2
+## (1d) Make a function to pick the total number of iterations (e.g., 10) and the values to print
+
+
+##########################################################################
+## Problem 2
+##########################################################################
+## (2a) Write a while loop to repeat 10 times and print a number starting at 1 and ending at 10
+## (2b) Write a while loop to repeat times and print a number starting at 11 and ending at 20
+## (2c) Write a while loop to repeat 10 times and print a number starting at 1 and ending at 2
+## (2d) Write a while loop to repeat at least 10 times AND not more than 20 times
+## (2e) Revise the loop above so that ends at a randomly selected iteration between 10 and 20
+## (2g) Make a function for (3e)
+
+
+##########################################################################
+## Problem 3
+##########################################################################
+## (3a) create an empty 10x10 matrix
+## (3b) fill the empty matrix with numbers randomly generated with the sample() function using a nested for loop
+
+
+##########################################################################
+## Problem 4 (advanced)
+##########################################################################
+## (4a) Write a while loop to repeat at least 10 times. Roll D6 dice each time the loop iterates and record the outcome. Make sure to specify an ending condition (see 2d above for one option).
+## (4b) Modify the loop so that it only ends once the dice rolls greater than three, 10 times in a row
+## (4c) Make the program so far a function
+## (4d) Modify the function so that you can change the value of the rolled dice condition (e.g., greater than 4 or greater than 5, etc).
+
+
+
+##########################################################################
+## Problem 5 (advanced)
+##########################################################################
+## (5) Complete exersicse 10.6 on page 213-214 in the Davies book.
